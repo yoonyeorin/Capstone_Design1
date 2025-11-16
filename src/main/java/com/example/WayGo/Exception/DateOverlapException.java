22e4dca548e52336dc8392c4e7a4bd9b1a610ef7
yoonjumi <yjmjymyjm@gmail.com>
2025-11-16 23:47:34 +0900

    Remove old itinerary files, prepare clean structure

--- src/main/java/com/example/WayGo/Exception/DateOverlapException.java
+++
@@ -1,7 +0,0 @@
-package com.example.WayGo.Exception;
-
-public class DateOverlapException extends RuntimeException {
-    public DateOverlapException(String message) {
-        super(message);
-    }
-}
